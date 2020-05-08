a08ea5bac52d956d99674a670d119912514569fe
Geoffrey Bosire Jr <gbosirejr@gmail.com>
2020-05-08 13:46:36 -0800

    added methods to show toast and increse counter | added string resources | App Complete

--- app/src/main/java/android/example/hellotoast/MainActivity.java
+++ app/src/main/java/android/example/hellotoast/MainActivity.java
@@ -3,12 +3,31 @@ package android.example.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int countUp = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+
+        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        countUp ++;
+        TextView count = findViewById(R.id.show_count);
+
+            count.setText(Integer.toString(countUp));
+
     }
 }
